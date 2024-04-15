e26c6bbdf81d0380bb71cd64a5afb830529bf54b
cinthia2982 <154032563+cinthia2982@users.noreply.github.com>
2024-04-15 18:08:39 -0400

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.evaluacion1proghibrida',
+  appName: 'evaluacion1proghibrida',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
